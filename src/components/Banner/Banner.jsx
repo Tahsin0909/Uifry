7a0617d46eee860d4066bed64a302ad94a1a1496
Tahsin0909 <tajisan789@gmail.com>
2024-06-05 02:16:24 +0600

    Added Banneer

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,9 +1,36 @@
-
+import { FaArrowRightLong } from "react-icons/fa6";
+import { IoPlayCircleOutline } from "react-icons/io5";
 const Banner = () => {
     return (
-        <div>
-            Banner
-        </div>
+        <section className="lg:max-w-screen-xl md:max-w-screen-md mx-auto lg:mt-16 md:mt-10 border border-red-400">
+            <div className="flex lg:flex-row flex-col">
+                <div className="space-y-8 w-[600px]">
+                    <div className="absolute top-8 -z-50 left-50">
+                        <img src="/liner.png" alt="" />
+                    </div>
+                    <p className="lg:text-6xl font-bold">Make the Best <br /> Financial Decisions</p>
+                    <p className="text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci ab ea nostrum. Voluptatem, omnis et! Inventore qui recusandae consectetur dignissimos.</p>
+                    <div className="flex lg:gap-8">
+                        <button className="css_button">
+                            <span>Get Started</span>
+                            <FaArrowRightLong size={20} />
+                        </button>
+                        <button className="css_button">
+                            <IoPlayCircleOutline size={20} />
+                            <span>Watch video</span>
+                        </button>
+                    </div>
+                    <img className="-translate-x-8" src="/banner slide.png" alt="" />
+                </div>
+
+                <div className="">
+                    <img className=" absolute -translate-y-20" src="/public/iPhone-13-Pro-Front.png" alt="" />
+                    <img className=" absolute translate-x-28 -translate-y-10 -z-30" src="/public/iPhone-13-Pro-Front (1).png" alt="" />
+                    <img className=" absolute translate-x-56 translate-y-10  -z-50" src="/public/iPhone-13-Pro-Front (2).png" alt="" />
+                </div>
+            </div>
+
+        </section>
     );
 };
 
--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -4,7 +4,7 @@ const NavBar = () => {
         <nav className="flex items-center  lg:max-w-screen-xl md:max-w-screen-md mx-auto css_navbar">
             <div className="flex items-center lg:flex-none flex-1">
 
-
+                {/* navlink for md and sm  */}
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
@@ -26,6 +26,8 @@ const NavBar = () => {
 
 
             </div>
+
+            {/* nav link for large device  */}
             <div className=" hidden lg:flex flex-1 lg:mx-20 lg:py-4 text-lg" >
                 <ul className="flex items-center gap-8 px-1">
                     <li><a className="css_shared_Color font-bold">Home</a></li>
@@ -34,6 +36,8 @@ const NavBar = () => {
                     <li><a>Features</a></li>
                 </ul>
             </div>
+
+            {/* download btn  */}
             <div className=" css_button">
                 <a className="">Download</a>
             </div>
