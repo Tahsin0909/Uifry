63480ed52e8cc204cffc7b9ef518762e4c0fa891
Tahsin0909 <tajisan789@gmail.com>
2024-06-04 23:41:30 +0600

    Navbar responsiveness completed

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,8 +1,8 @@
 
 const NavBar = () => {
     return (
-        <nav className="flex items-center max-w-screen-xl mx-auto css_navbar">
-            <div className="flex items-center">
+        <nav className="flex items-center  lg:max-w-screen-xl md:max-w-screen-md mx-auto css_navbar">
+            <div className="flex items-center lg:flex-none flex-1">
 
 
                 <div className="dropdown">
@@ -10,7 +10,7 @@ const NavBar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Home</a></li>
+                        <li><a className="css_shared_Color font-bold">Home</a></li>
                         <li><a>About Us</a></li>
                         <li><a>Pricing</a></li>
                         <li><a>Features</a></li>
@@ -18,8 +18,8 @@ const NavBar = () => {
                 </div>
 
                 {/* logo  */}
-                <div className=" flex items-center ">
-                    <img className="lg:w-10" src="/logo.png" alt="" />
+                <div className=" flex items-center">
+                    <img className="lg:w-10 md:w-8 w-8" src="/logo.png" alt="" />
                     <span className="text-xl font-bold">uifry</span>
                     <span className="text-[8px]">TM</span>
                 </div>
